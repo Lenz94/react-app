ef9a749c666c9cfc03251bb3df088f890f5fe09a
Enzo Marcani <enzo_marcani@outlook.com>
2025-03-22 09:13:21 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+import Button from "./components/Button";
+import Alert from "./components/Alert";
+import Fixtures from "./components/Fixtures";
+
+/*import ListGroup from "./components/ListGroup";
+
+function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
+}*/
+
+function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+  return (
+    <div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>Hello World</Alert>
+      )}
+      <Button color="primary" onClick={() => setAlertVisibility(true)}>
+        My button
+      </Button>
+      <Fixtures></Fixtures>
+    </div>
+  );
+}
+
+export default App;
